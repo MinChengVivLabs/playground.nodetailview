1ab8bc1a577bcd58955fd9ce7828893ea20e900d
Min Cheng <mcheng@sixfivelabs.com>
2020-03-26 13:32:38 -0700

    initial commit

---
+++ code/CreateTitle.js
@@ -0,0 +1,8 @@
+module.exports.function = function createTitle () {
+  return [
+  "title a",
+  "title b", 
+  "title c", 
+  "title d",
+  ]
+}
---
+++ code/PlayTitle.js
@@ -0,0 +1,3 @@
+module.exports.function = function playTitle (title) {
+  return "now playing " + title;
+}
